f8fad70652f7bfe26dd3e2a487d9af08dc3e3f5a
marwankhalil <marwank@vt.edu>
2025-03-31 22:19:57 -0400

    dynamic id page

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Uncomment the following line to build a static site.
-  output: "export",
-
   reactStrictMode: true,
 };
 
--- src/pages/matches/[id].js
+++ src/pages/matches/[id].js
@@ -54,7 +54,6 @@ export default function MatchBetPage() {
         router.push('/404');
       }
     }
-
     if (id) {
       loadMatch();
     }
